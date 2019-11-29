f5873ff6a3b0716c35ec2e1826c8eb67c5e98efb
Ta-von <55704880+Ta-von@users.noreply.github.com>
2019-11-29 08:52:08 -0800

    Update session.php

--- session.php
+++ session.php
@@ -1,13 +1,13 @@
 <?php
 //mysqli_connect() pens a new connection to the MySql server.
-$conn = mysqli_connect("localhost", "root", "whatever the server is");
+$conn = mysqli_connect("localhost", "root", "schema.sql");
 session_start();// Starting Session
 
 //Storing session
 $user_check= $_SESSION['login_user'];
 
 //query to get the comlete information of the current user
-$query="SELECT Email from user where Email= '$user_check'";
+$query="SELECT Email from User where Email= '$user_check'";
 $ses_sql=mysqli_query($conn,$query);
 $row=mysqli_fetch_assoc($ses_sql);
 $login_session=$row['Email'];
